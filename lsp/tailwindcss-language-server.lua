a5df10918140fc64144854f7d64aa35a0cf47fe4
Glicio Pereira Uchoa <glicioo@outlook.com>
2025-05-30 20:25:46 -0300

    tailwind lsp setup

---
+++ lsp/tailwindcss-language-server.lua
@@ -0,0 +1,6 @@
+
+return {
+	cmd = { "tailwindcss-language-server", "--stdio" },
+	filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  root_markers = {'package.json', '.git', '.yarnrc', 'tsconfig.json'},
+}
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,4 +1,15 @@
-local servers = { "luals", "typescript-language-server", "pyright", "bashls", "jsonls", "vscode-eslint-language-server", "emmet_ls", "yamlls", "eslint" }
+local servers = {
+	"luals",
+	"typescript-language-server",
+	"pyright",
+	"bashls",
+	"jsonls",
+	"vscode-eslint-language-server",
+	"emmet-ls",
+	"yamlls",
+	"eslint",
+	"tailwindcss-language-server",
+}
 
 for _, server in ipairs(servers) do
 	vim.lsp.enable(server)
@@ -12,16 +23,16 @@ for _, server in ipairs(servers) do
 	})
 end
 
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local opts = { buffer = args.buf }
 
-		vim.keymap.set('n', '<C-Space>', '<C-x><C-o>', opts)
-		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-		vim.keymap.set({ 'n', 'x' }, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+		vim.keymap.set("n", "<C-Space>", "<C-x><C-o>", opts)
+		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "gq", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
 
-		vim.keymap.set('n', 'grt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-		vim.keymap.set('n', 'grd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+		vim.keymap.set("n", "grt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "grd", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
 	end,
 })
 
