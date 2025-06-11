2beb37448500e7b72e42a56348fd961787d1e3d8
Glicio Pereira Uchoa <glicioo@outlook.com>
2025-06-11 14:37:42 -0300

    removed ai slop code

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -24,35 +24,7 @@ for _, server in ipairs(servers) do
 	})
 end
 
---start eslint
-vim.api.nvim_create_user_command("EslintStart", function()
-	local server = require("config.lsp.vscode-eslint-language-server")
-	local client_id = vim.lsp.start(server)
-	print("Started eslint LSP server with client id: " .. client_id)
-end, {})
 
---stop eslint
-vim.api.nvim_create_user_command("EslintStop", function()
-	for _, client in pairs(vim.lsp.get_clients()) do
-		if client.name == "vscode-eslint-language-server" then
-			client:stop(false)
-			print("Stopped LSP server: " .. client.name)
-		end
-	end
-end, {})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local opts = { buffer = args.buf }
-
-		vim.keymap.set("n", "<C-Space>", "<C-x><C-o>", opts)
-		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-		vim.keymap.set({ "n", "x" }, "gq", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-
-		vim.keymap.set("n", "grt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-		vim.keymap.set("n", "grd", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-	end,
-})
 
 vim.diagnostic.config({
 	virtual_text = {
